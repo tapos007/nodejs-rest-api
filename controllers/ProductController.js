464f3fbe3ef71c0cd99636af2450d420ac1fe5a3
Biswa Nath Ghosh <tapos.aa@gmail.com>
2018-04-16 18:20:16 +0600

    product routes all functional workable

--- controllers/ProductController.js
+++ controllers/ProductController.js
@@ -1,42 +1,98 @@
 const ProductModel = require('../models/ProductModel');
-exports.create = (req, res) => {
-    const product = {
-        name:req.body.name,
-        price:req.body.price
-    };
-    res.status(200).json({
-        message : 'Handling POST requests to /products',
-        createdProduct:product
+require('express-async-errors');
+exports.create = async (req, res) => {
+
+    const product = new ProductModel({
+        name: req.body.name,
+        price: req.body.price
     });
+    try {
+        await  product.save();
+        res.status(201).json({
+            message: 'product insert successfully',
+            id: product._id,
+            createdProduct: product
+        });
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+
+
 };
 
 // Retrieve and return all notes from the database.
-exports.findAll = (req, res) => {
-    res.status(200).json({
-        message : 'Handling GET requests to /products'
-    });
+exports.findAll = async (req, res) => {
+    try {
+        const product = await ProductModel.find({});
+        if(product.length>0){
+            res.status(200).json(product);
+        }else{
+            res.status(404).json({
+                message:"no entries found"
+            });
+        }
+
+    } catch (e) {
+        res.status(500).json({
+            error: err
+        });
+    }
+
+
 };
 
 // Find a single note with a noteId
-exports.findOne = (req, res) => {
+exports.findOne = async (req, res) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message : `Handling GET requests to /products ${id}`
-    });
+    try {
+        const product = await ProductModel.findById(id);
+        if (product == null) {
+            res.status(404).json({
+                message: `no valid product found for this id: ${id}`,
+            });
+        } else {
+            res.status(200).json({
+                message: `Handling GET requests to /products ${id}`,
+                product: product
+            });
+        }
+
+    } catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+
 };
 
 // Update a note identified by the noteId in the request
-exports.update = (req, res) => {
+exports.update = async (req, res) => {
     const id = req.params.productId;
+    try {
+        const  product = await ProductModel.update({_id:id},{$set:{name:req.body.name,price:req.body.price}});
+        res.status(200).json(product);
+    }catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
     res.status(200).json({
-        message : `Updated Product!`
+        message: `Updated Product!`
     });
 };
 
 // Delete a note with the specified noteId in the request
-exports.delete = (req, res) => {
+exports.delete = async (req, res) => {
     const id = req.params.productId;
-    res.status(200).json({
-        message : `Delete Product!`
-    });
+    try {
+       const  product = await ProductModel.remove({_id:id});
+        res.status(200).json(product);
+    }catch (err) {
+        res.status(500).json({
+            error: err
+        });
+    }
+
 };
